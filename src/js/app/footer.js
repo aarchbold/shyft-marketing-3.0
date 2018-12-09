a8593d59204e6f9a299006f480eb2e5d0f476a4e
Andrew Archbold <pulsegrenade@gmail.com>
2018-12-09 13:06:27 -0800

    #time 1h 0m adding topnav and interactions

--- src/js/app/footer.js
+++ src/js/app/footer.js
@@ -1,19 +1,19 @@
-function getParam(name) {
-    SCH = document.location.search;
-    if(window['W3T'] && (W3T['MORE_ARGS'] != "")) {
-        SCH += "&" + W3T['MORE_ARGS'];
-    }
-    SCH = "?&" + SCH.substring(1,SCH.length);
-    // alert('SCH = ' + SCH);
-    var start = SCH.indexOf("&" + name+"=");
-    var len = start+name.length+2;
-    if ((!start) && (name != SCH.substring(0,name.length))) return("");
-    if (start == -1) return "";
-    var end = SCH.indexOf("&",len);
-    if (end == -1) end = SCH.length;
-    // alert('finished getting parm ' + name);
-    return unescape(SCH.substring(len,end));
-}
+// function getParam(name) {
+//     SCH = document.location.search;
+//     if(window['W3T'] && (W3T['MORE_ARGS'] != "")) {
+//         SCH += "&" + W3T['MORE_ARGS'];
+//     }
+//     SCH = "?&" + SCH.substring(1,SCH.length);
+//     // alert('SCH = ' + SCH);
+//     var start = SCH.indexOf("&" + name+"=");
+//     var len = start+name.length+2;
+//     if ((!start) && (name != SCH.substring(0,name.length))) return("");
+//     if (start == -1) return "";
+//     var end = SCH.indexOf("&",len);
+//     if (end == -1) end = SCH.length;
+//     // alert('finished getting parm ' + name);
+//     return unescape(SCH.substring(len,end));
+// }
 
 
 $.fn.handleTextLink = function() {
--- src/js/app/topnav.js
+++ src/js/app/topnav.js
@@ -16,40 +16,6 @@
 // }
 
 
-$.fn.handleTextLink = function() {
-    var $container = $(this),
-        $button = $('.button', $container),
-        $form = $('input', $container);
-    
-    $button.click(function(e){
-        alert('TODO: add functionality for sending text links')
-    });
-}
-
-$(function(){
-    console.log($);
-    console.log('hello');
-    
-    $('#downloadLinkForm').handleTextLink();
-});
-// function getParam(name) {
-//     SCH = document.location.search;
-//     if(window['W3T'] && (W3T['MORE_ARGS'] != "")) {
-//         SCH += "&" + W3T['MORE_ARGS'];
-//     }
-//     SCH = "?&" + SCH.substring(1,SCH.length);
-//     // alert('SCH = ' + SCH);
-//     var start = SCH.indexOf("&" + name+"=");
-//     var len = start+name.length+2;
-//     if ((!start) && (name != SCH.substring(0,name.length))) return("");
-//     if (start == -1) return "";
-//     var end = SCH.indexOf("&",len);
-//     if (end == -1) end = SCH.length;
-//     // alert('finished getting parm ' + name);
-//     return unescape(SCH.substring(len,end));
-// }
-
-
 $.fn.handleTopNav = function() {
     var $container = $(this),
         $mainMenu = $('#topnavMenu', $container),
