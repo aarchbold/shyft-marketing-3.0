58923a8a2db7d1158a432e29e12a3438543b14a6
Andrew Archbold <pulsegrenade@gmail.com>
2018-12-23 22:21:43 -0800

    #time 2h 0m adding animations and finishing testimonial section

--- site/static/js/app.js
+++ site/static/js/app.js
@@ -1,16 +1,46 @@
+function isElementInViewport (el) {
+
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
+
 $.fn.flyInFromRight = function() {
-    $this = $(this);
+    $this = $(this),
+        $elements = $('.fly-from-right', $this);
 
     window.setTimeout(function() {
-        $this.removeClass('fly-from-right');
-    },1000)
+        $elements.removeClass('fly-from-right');
+    },400);
 
-    console.log($this);
-    console.log('really?');
+
+    // console.log(isElementInViewport($this));
+    // console.log($this);
+    // console.log($elements);
+    // console.log($this);
+
+    // $(window).on('DOMContentLoaded load resize scroll', function() {
+    //     console.log(isElementInViewport($this));
+    //     if (isElementInViewport($this)) {
+            
+    //     } else {
+    //         //$elements.addClass('fly-from-right');
+    //     }
+    // }); 
 }
 
 $(function(){    
-    $('.fly-from-right').flyInFromRight();
+    $('.home-hero__inner').flyInFromRight();
+    //$('.home-solutions').flyInFromRight();
 });
 // function getParam(name) {
 //     SCH = document.location.search;
